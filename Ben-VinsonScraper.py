1e4e2c2aaca6d126fe4fb8c426e01507a3f95c5b
Ben B <bbui@ualberta.ca>
2024-11-09 11:59:13 -0700

    heh

--- Ben-VinsonScraper.py
+++ Ben-VinsonScraper.py
@@ -2,16 +2,16 @@ import urllib.request
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://apps.ualberta.ca/catalogue/course/cmput"
+def benWork(soup):
+    # looks for "div" and looks for "class" called course first
+    text = soup.find("div", {"class": "course first"})
+    text = text.get_text()
+    print(text)
+    print(type(text))
 
-r = requests.get(url)
+def main():
+    url = "https://apps.ualberta.ca/catalogue/course/cmput"
 
-soup = BeautifulSoup(r.content, 'html.parser')
-
-# looks for "div" and looks for "class" called course first
-
-text = soup.find("div", {"class": "course first"})
-text = text.get_text()
-print(text)
-print(type(text))
-# print(test.prettify())
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    benWork(soup)
