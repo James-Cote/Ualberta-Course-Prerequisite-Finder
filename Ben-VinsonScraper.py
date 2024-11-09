c7add8ed1c4b14ba99129bdc8da17e4e835df553
Ben B <bbui@ualberta.ca>
2024-11-09 12:06:35 -0700

    can stringify the resultset tags

--- Ben-VinsonScraper.py
+++ Ben-VinsonScraper.py
@@ -4,10 +4,15 @@ from bs4 import BeautifulSoup
 
 def benWork(soup):
     # looks for "div" and looks for "class" called course first
-    text = soup.find("div", {"class": "course first"})
-    text = text.get_text()
-    print(text)
-    print(type(text))
+    text = soup.find_all("div", {"class": "course first"})
+    # print(type(text))
+
+    str;
+    for i in range(len(text)):
+        elementTag = text[i].get_text()
+        print(elementTag)
+        
+    # print(type(text))
 
 def main():
     url = "https://apps.ualberta.ca/catalogue/course/cmput"
@@ -15,3 +20,5 @@ def main():
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
     benWork(soup)
+
+main()
